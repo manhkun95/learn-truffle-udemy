a0a2091d9f64d7d184378844bece67e258293629
manhkun95 <nguyenduymanh0406@gmail.com>
2018-10-04 17:45:11 +0700

    update truffle test

--- test/01_fixed_supply_token_test.js
+++ test/01_fixed_supply_token_test.js
@@ -2,6 +2,45 @@ var fixedSupplyToken = artifacts.require("./FixedSupplyToken.sol");
 
 contract('MyToken', function (accounts) {
   it("should verify this in here", function () {
-
+    var _totalSupply;
+    var myTokenInstance;
+    return fixedSupplyToken.deployed().then(function (instance) {
+      myTokenInstance = instance;
+      return myTokenInstance.totalSupply.call();
+    }).then(function (totalSupply) {
+      _totalSupply = totalSupply;
+      return myTokenInstance.balanceOf(accounts[0]);
+    }).then(function (balanceAccountOwner) {
+      assert.equal(balanceAccountOwner.toNumber(), _totalSupply.toNumber(), "Total Amount of tokens is owned by owner");
+    })
   });
+
+  it("Second account should own no tokens", function () {
+    var myTokenInstance;
+    return fixedSupplyToken.deployed().then(function (install) {
+      myTokenInstance = install;
+      return myTokenInstance.balanceOf(accounts[1]);
+    }).then(function (balanceAccountOwner) {
+      assert.equal(balanceAccountOwner.toNumber(), 0, "Total Amount of tokens is owned by some other address")
+    })
+  });
+
+  // it("should send token correctly", function () {
+  //   var token;
+
+  //   var account_one = accounts[0];
+  //   var account_two = accounts[1];
+
+  //   var account_one_starting_balance;
+  //   var account_two_starting_balance;
+  //   var account_one_ending_balance;
+  //   var account_two_ending_balance;
+
+  //   var amount = 10;
+
+  //   return fixedSupplyToken.deployed().then(function (instance) {
+  //     token = instance;
+  //     return token.balanceOf
+  //   })
+  // });
 })
